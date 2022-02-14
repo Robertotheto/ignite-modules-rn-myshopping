e18d184cf4a16984b55e05931613b170da26a732
Roberto dos Santos Filho <robertodossantosfilho1001@gmail.com>
2022-02-13 22:04:39 -0400

    Creating an account with email and password.

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import auth from '@react-native-firebase/auth';
 import { Container, Account, Title, Subtitle } from './styles';
 import { ButtonText } from '../../components/ButtonText';
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
+import { Alert } from 'react-native';
 
 export function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   async function handleAnonymous() {
     const { user } = await auth().signInAnonymously();
     console.log(user);
+  }
+  async function handleCreateAccount() {
+    auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(() => {
+        Alert.alert('Usuário criado com sucesso!')
+      })
   }
   return (
     <Container>
@@ -18,18 +28,20 @@ export function SignIn() {
       <Input
         placeholder="e-mail"
         keyboardType="email-address"
+        onChangeText={setEmail}
       />
 
       <Input
         placeholder="senha"
         secureTextEntry
+        onChangeText={setPassword}
       />
 
       <Button title="Entrar" onPress={handleAnonymous} />
 
       <Account>
         <ButtonText title="Recuperar senha" onPress={() => { }} />
-        <ButtonText title="Criar minha conta" onPress={() => { }} />
+        <ButtonText title="Criar minha conta" onPress={handleCreateAccount} />
       </Account>
     </Container>
   );
