2bcef45166ed33ca1fb613625b46861a982bdc17
Roberto dos Santos Filho <robertodossantosfilho1001@gmail.com>
2022-02-13 21:03:17 -0400

    Getting information from the file.

--- src/screens/Receipts/index.tsx
+++ src/screens/Receipts/index.tsx
@@ -10,6 +10,7 @@ import { File, FileProps } from '../../components/File';
 export function Receipts() {
   const [photo, setPhoto] = useState<FileProps[]>([])
   const [photoSelected, setPhotoSelected] = useState('')
+  const [photoInfo, setPhotoInfo] = useState('')
 
   useEffect(() => {
     storage().ref('images').list().then((result) => {
@@ -27,6 +28,9 @@ export function Receipts() {
   async function handleShowImage(path: string) {
     const urlImage = await storage().ref(path).getDownloadURL();
     setPhotoSelected(urlImage);
+
+    const info = await storage().ref(path).getMetadata();
+    setPhotoInfo(`Upload realizado em ${info.timeCreated}`)
   }
 
   return (
@@ -36,7 +40,7 @@ export function Receipts() {
       <Photo uri={photoSelected} />
 
       <PhotoInfo>
-        Informações da foto
+        {photoInfo}
       </PhotoInfo>
 
       <FlatList
